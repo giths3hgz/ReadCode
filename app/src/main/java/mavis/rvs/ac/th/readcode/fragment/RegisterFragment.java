9f2dff684c0a9fa53ad741bbd1d7618cd2382956
gitMavis <hs3hgz@gmail.com>
2018-03-21 14:49:17 +0700

    21March14:49

--- app/src/main/java/mavis/rvs/ac/th/readcode/fragment/RegisterFragment.java
+++ app/src/main/java/mavis/rvs/ac/th/readcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package mavis.rvs.ac.th.readcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import mavis.rvs.ac.th.readcode.MainActivity;
 import mavis.rvs.ac.th.readcode.R;
 
 /**
@@ -15,6 +17,26 @@ import mavis.rvs.ac.th.readcode.R;
 
 public class RegisterFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create ToolBar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+//        Show Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }//Main Method
 
     @Nullable
     @Override
